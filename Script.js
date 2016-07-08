88c05844c95621a8936820c72538fe32274f3ce0
itsgithub <learn2code@gmail.com>
2016-07-08 10:53:20 -0500

    Added but styles and pointer
    Changes done to sort by clicking on table header

--- Script.js
+++ Script.js
@@ -29,4 +29,23 @@ var app = angular
            $scope.employees = employees;
            $scope.sortColumn = "name";
 
+           $scope.reverseSort = false;
+
+           $scope.sortData = function (column) {
+               $scope.reverseSort = ($scope.sortColumn == column) ?
+                   !$scope.reverseSort : false;
+               $scope.sortColumn = column;
+           }
+
+           $scope.getSortClass = function (column) {
+
+               if ($scope.sortColumn == column) {
+                   return $scope.reverseSort
+                     ? 'arrow-down'
+                     : 'arrow-up';
+               }
+
+               return '';
+           }
        });
+
